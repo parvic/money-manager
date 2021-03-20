ac97e2254e5468dc78b7f667b5ad10ec239e746c
victorprnt <victorprnt@gmail.com>
2021-03-20 14:25:42 -0300

    Created fake API: miragejs and axios

--- src/components/TransactionsTable.tsx
+++ src/components/TransactionsTable.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { api } from "../services/api";
 import * as S from "../styles/components/TransactionsTable";
 
 export function TransactionsTable() {
+
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => console.log(response.data))
+  }, [])
+
   return (
     <S.Container>
       <table>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createServer } from "miragejs";
+
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Grocery',
+          amount: '400',
+          type: 'Deposit',
+          category: 'Food',
+          createdAt:new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
